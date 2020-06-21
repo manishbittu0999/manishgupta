23263c12913daeeb9dc3d41dd67dd778385b981d
Manish Gupta <manishbittu0999@users.noreply.github.com>
2020-06-21 21:37:59 +0530

    BugFixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Background from './Background.png';
 
 function App() {
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <header className="App-header">
+        <img src={Background} alt='website logo' />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2>
           Manish Gupta
@@ -20,6 +23,13 @@ function App() {
         >
           Learn React
         </a> */}
+        <br /><br /><br /><br /><br /><br /><br /><br />
+        <a href="https://www.twitter.com/i_am_manish09"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+        {/* <i class="fa fa-facebook-square" aria-hidden="true"></i> */}
+        <hr />
+        <div className="copyright">
+          <p1>© 2020, Manish Gupta</p1>
+        </div>
       </header>
     </div>
   );
